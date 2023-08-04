5f62137c4a8b75fa882ce8b0083ff147933b3d12
Sanket Patil <sanket.9006@gmail.com>
2023-08-04 16:45:49 +0530

    Delete externall library used

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,40 +1,24 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
+import React from 'react';
 import { FaBars } from 'react-icons/fa'; // Importing the Font Awesome Bars icon
-import { Input } from '@mui/material';
-
 
 export default function ButtonAppBar({ text, setText }) {
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    >
-                        <FaBars /> {/* Using the Font Awesome Bars icon */}
-                    </IconButton>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        randstad risesmart
-                    </Typography>
-                    <Input
+        <div style={{ flexGrow: 1 }}>
+            <div style={{ backgroundColor: '#1976D2', padding: '16px' }}>
+                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                        <FaBars style={{ fontSize: '24px', marginRight: '10px', color: 'white' }} />
+                        <h2 style={{ margin: 0, color: 'white' }}>randstad risesmart</h2>
+                    </div>
+                    <input
                         type="text"
                         placeholder="Search..."
                         value={text}
                         onChange={(e) => { setText(e.target.value) }}
-                        style={{ backgroundColor: 'white', padding: 2, borderRadius: 5 }} // Add white background color
+                        style={{ backgroundColor: 'white', padding: '4px', borderRadius: '5px', border: 'none' }}
                     />
-                </Toolbar>
-            </AppBar>
-        </Box>
+                </div>
+            </div>
+        </div>
     );
 }
